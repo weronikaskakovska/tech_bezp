63e168d7536fb9dce8dedc76dea6895b0789ed43
Weronika Skakovska <skakovskaweronika@gmail.com>
2026-02-03 23:36:10 +0100

    final touches

--- warehouse/models/user.js
+++ warehouse/models/user.js
@@ -24,11 +24,11 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // password encryption before save
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
+
 
 // comparing password method
 userSchema.methods.comparePassword = async function(password) {
--- warehouse/server.js
+++ warehouse/server.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false})) //otherwise we can't add form url-encoded way
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: true,
   credentials: true
