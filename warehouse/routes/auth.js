abadbf050828abd1c33893314ba8ff9d85eccb52
Weronika Skakovska <skakovskaweronika@gmail.com>
2026-06-02 12:09:40 +0200

    fixed case-sensitive import for user model :)

--- warehouse/routes/auth.js
+++ warehouse/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 const { authMiddleware } = require('../middleware/auth');
 const { keycloak } = require('../server');
 
